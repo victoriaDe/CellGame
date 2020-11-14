b47cead311603c88a1fe4e5d8b116d4f3654eaa9
Dzmitry Rudenka <drudenka@workfusion.com>
2020-11-14 19:51:43 +0300

    slider test

--- jsTest.js
+++ jsTest.js
@@ -1,58 +1,15 @@
-/*
-let questionContainer = document.getElementById('question__popup__container');
-let questionContent = document.getElementById('question__popup__content');
-let questionBtn;
-let questionInput;
 let btn = document.getElementById('siskiIDrakony');
-btn.addEventListener('click', () => questionPopupOpen(player));
+btn.addEventListener('click', winPopupOpen);
 
+let winPopupContainer = document.getElementById('winPopup__container');
 
-function questionPopupOpen(player) {
-    questionBtn = document.getElementById('question__btn__Check');
-    questionInput = document.getElementById('answer_input');
-    questionInput.addEventListener('input', () => checkAnswer(player), {once: true});
+function winPopupOpen() {
+    winPopupContainer.classList.remove('winPopupContainer_close');
 
-    setRandomQuestion();
-
-    questionContainer.classList.remove('closedQuestion__popup__container');
-    questionContent.classList.remove('closedQuestion__popup__content');
-    questionContainer.classList.add('openQuestion__popup__container');
-    questionContent.classList.add('openQuestion__popup__content');
-}
-
-
-function checkAnswer(player) {
-    if (questionInput.value.length === 0) {
-        questionBtn.removeEventListener('click', () => onSubmit(player), {once: true});
-    } else {
-        questionBtn.addEventListener('click', () => onSubmit(player), {once: true});
-    }
-}
-
-function onSubmit(player) {
-    console.log(player.name);
-    if (answerInput.value.trim().toLowerCase() === correctAnswer.trim().toLowerCase()) {
-        questionContent.innerHTML = '<h2>Correct! +50 coins!</h2>';
-        player.element
-            .score += 50;
-    } else {
-        questionContent.innerHTML = '<h2>Oops...The correct answer is:<br>' + correctAnswer + '<br>-10 coins!</h2>';
-        player.element.score -= 10;
-    }
-    closeQuestionPopup();
-}
-
-function closeQuestionPopup() {
-    setTimeout(questionPopupClose, 4000);
+    setTimeout(winPopupClose, 5000);
 
 }
 
-function questionPopupClose() {
-    questionContainer.classList.add('closedQuestion__popup__container');
-    questionContent.classList.add('closedQuestion__popup__content');
-    questionContainer.classList.remove('openQuestion__popup__container');
-    questionContent.classList.remove('openQuestion__popup__content');
+function winPopupClose() {
+    winPopupContainer.classList.add('winPopupContainer_close');
 }
-
-
-//----*/
---
+++ test.js
@@ -0,0 +1,10 @@
+let string2 = "Lol";
+let strings = ["lol1", "lol2"];
+// console.log(typeof string);
+console.log(typeof strings);
+
+if (typeof string2 === string) {
+    console.log(1);
+} else {
+    console.log(2);
+}
